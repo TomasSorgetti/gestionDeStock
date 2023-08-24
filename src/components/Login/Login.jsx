ff75db269270da664a11adcb083cb91e01bce949
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-08-24 20:46:28 -0300

    login css

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,36 +1,89 @@
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import axios from "axios";
+import Logo from "../../assets/Logo.png";
 const Login = () => {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:3001/login/validate", form);
+    const authHeader = res.headers["auth-token"];
+    console.log("Token:", authHeader);
+  };
   return (
     <section className="flex items-center h-screen">
-      <article className="h-full w-6/12 bg-blue-600"></article>
-      <article className="h-full w-6/12 flex flex-col justify-center bg-blue-200">
-        <div className="px-20 flex flex-col gap-6">
-          <div className="">
-            <h2>
-              Inicia sesión o <Link to="/register">registrate</Link>
-            </h2>
-          </div>
-          <span>Ingresa tus datos</span>
-          <div>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Correo Electrónico"
-              className="mb-3"
-            >
-              <Form.Control type="email" placeholder="name@example.com" />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingPassword" label="Contraseña">
-              <Form.Control type="password" placeholder="Contraseña" />
-            </FloatingLabel>
-          </div>
-          <div className="w-full flex justify-center">
-            <button className="bg-blue-600 px-10 py-2 rounded w-full">
-              Entrar
-            </button>
-          </div>
+      <article className="h-full w-6/12 bg-primaryColor flex flex-col items-center justify-center">
+        <img className="" src={Logo} alt="logo" />
+        <p className="absolute left-40 bottom-6 text-[26px] font-bold text-white">
+          Tené el control de tu negocio
+        </p>
+      </article>
+
+      <article className="h-full w-6/12 flex flex-col justify-center bg-white">
+        <div className="px-20 flex flex-col gap-6 w-11/12 mx-auto">
+          <h2 className="text-primaryColor font-bold text-[30px]">
+            Inicia sesión o registrate
+          </h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-10">
+            <div className="flex flex-col gap-3">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Correo Electrónico"
+                className="text-inputTextColor"
+              >
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  value={form.email}
+                  name="email"
+                  onChange={handleChange}
+                  className="text-inputBorderColor"
+                />
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Contraseña"
+                className="text-inputTextColor"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  value={form.password}
+                  name="password"
+                  onChange={handleChange}
+                  className="text-inputBorderColor"
+                />
+              </FloatingLabel>
+            </div>
+            <div className="w-full flex justify-center">
+              <button
+                type="submit"
+                className="bg-primaryColor px-10 py-3 text-white rounded w-8/12 font-bold text-[16px]"
+              >
+                Entrar
+              </button>
+            </div>
+            <p className="text-center text-textColor">
+              ¿Eres nuevo/a?{" "}
+              <Link
+                to="/register"
+                className="font-medium text-primaryColor cursor-pointer"
+              >
+                Registrate
+              </Link>
+            </p>
+          </form>
         </div>
       </article>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#009ED0",
+        textColor: "#535353",
+        inputTextColor: "#4F4F4F80",
+        inputBorderColor: "#CDCDCD",
+      },
+      fontFamily: {},
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+      "3xl": "1800px",
+    },
   },
   plugins: [],
 };
